a411fea6b9e23a55160c279a7bc5afdcf3061233
Aaron Pittman <pittmana687@gmail.com>
2020-05-24 19:20:21 -0600

    Trying to connect backend with product flavors

--- app/src/free/java/com/udacity/gradle/flavors/free/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/flavors/free/MainActivityFragment.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.builditbigger.R;
