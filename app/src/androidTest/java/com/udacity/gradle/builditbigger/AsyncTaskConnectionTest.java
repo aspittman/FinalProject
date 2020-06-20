8c0f3e6b3cd2ab3556df955ed71e8c4b68ad11dc
Aaron Pittman <pittmana687@gmail.com>
2020-06-20 01:34:02 -0600

    First Test Complete

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskConnectionTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskConnectionTest.java
@@ -3,25 +3,39 @@ package com.udacity.gradle.builditbigger;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import androidx.test.filters.LargeTest;
 import androidx.test.filters.SmallTest;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
-@SmallTest
+@LargeTest
 public class AsyncTaskConnectionTest {
+    @Rule
+    public ActivityTestRule activityRule =
+            new ActivityTestRule(MainActivity.class);
+
 
     @Test
     public void TestAsyncTaskResponse() {
 
-        new EndpointsAsyncTask();
-        AsyncTask.execute((Runnable) this);
+        onView(withId(R.id.tell_joke_button))
+                .check(matches(withText("Tell Joke")));
 
     }
 }
